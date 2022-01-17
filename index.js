929ff62d6bd49bbaac530e035cb738f637b70f61
Luiz Henrique <luishenriqueee2010@hotmail.com>
2022-01-16 21:51:35 -0300

    add mudancas no index e joi ao package.json

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const PORT = 3000;
 const app = express();
-const root = require('./controllers/root');
 
+const root = require('./controllers/root');
+const err = require('./middlewares/errors');
+
+const PORT = 3000;
 app.use(express.json());
 
-app.use(root, '/');
+app.use('/', root);
+app.use(err);
 
 app.listen(PORT, () => {
   console.log('app running on port 3000');
