d8385462f1265950b35cb0ece5d52077e8432b1e
Luiz Henrique <luishenriqueee2010@hotmail.com>
2022-02-06 18:23:01 -0300

    test: change the cors permission

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'chrome-extension://gckhpdjepjfehifhnpbpjmndpkjgfgia',
   methods: ['POST'],
 }));
 
